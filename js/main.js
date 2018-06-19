8e116a13e1fd145d4d2a9ae7d72351c3574090a4
sammy1997 <sombuddha2016@gmail.com>
2018-06-19 23:39:54 +0530

    Added graph

--- js/main.js
+++ js/main.js
@@ -1,48 +1,248 @@
-
-var trace1 = {
-  x: [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
-  y: [ 350, 430, 474, 526, 488, 537, 500, 439],
-  name: 'Rest of world',
-  marker: {color: 'rgb(55, 83, 109)'},
-  type: 'bar'
-};
-
-var trace2 = {
-  x: [ 2017, 2018, 2019, 2020],
-  y: [  3.40, 4.03, 5.49, 4.99],
-  name: 'China',
-  marker: {color: 'rgb(26, 118, 255)'},
-  type: 'bar'
-};
-
-var data = [ trace2];
-
-var layout = {
-  title: 'Years',
-  xaxis: {tickfont: {
-      size: 14,
-      color: 'rgb(107, 107, 107)'
-    }},
-  yaxis: {
-    title: 'Predicted Yeild ( in million tonnes )',
-    titlefont: {
-      size: 16,
-      color: 'rgb(107, 107, 107)'
+var myTheme = {
+  palette:{
+    line:[
+        ['#FBFCFE', '#00BAF2', '#00BAF2', '#00a7d9'], /* light blue */
+    ]
+  },
+  graph:{
+    backgroundColor:'#242f3e',
+    title: {
+      fontFamily: 'Lato',
+      fontSize: 14,
+      // border: "1px solid black",
+      padding: "15",
+      fontColor : "#1E5D9E",
+      adjustLayout:true
     },
-    tickfont: {
-      size: 14,
-      color: "rgb(107, 107, 107)"
+    subtitle: {
+      fontFamily: 'Lato',
+      fontSize: 12,
+      fontColor: "#777",
+      padding: "5"
+    },
+    plot: {
+      backgroundColor:'#FBFCFE',
+      marker: {
+        size: 4
+      }
+    },
+   tooltip : {
+        visible:true,
+        text: "%kl<br><span style='color:%color'>%t: </span>%v<br>",
+        backgroundColor : "white",
+        borderColor : "#e3e3e3",
+        borderRadius : "5px",
+        bold : true,
+        fontSize : "12px",
+        fontColor : "#2f2f2f",
+        textAlign : 'left',
+        padding : '15px',
+        shadow : true,
+        shadowAlpha : 0.2,
+        shadowBlur : 5,
+        shadowDistance : 4,
+        shadowColor : "#a1a1a1"
+    },
+    plotarea: {
+      backgroundColor: 'transparent',
+      borderRadius: "0 0 0 10",
+      borderRight: "0px",
+      borderTop: "0px",
+      margin:"dynamic",
+      borderLeft:'1px solid #1E5D9E',
+      borderBottom:'1px solid #1E5D9E',
+    },
+    scaleX: {
+      zooming:true,
+      offsetY: -20,
+      lineWidth: 0,
+      padding: 20,
+      margin: 20,
+      label:{
+        text:"Scale-X"
+      },
+      item: {
+        padding: 5,
+        fontColor : "#1E5D9E",
+        fontFamily: 'Montserrat',
+        rules: [
+          {
+            rule: "%i == 0",
+            visible: false
+          }
+        ]
+      },
+      tick: {
+        lineColor: '#D1D3D4',
+        rules: [
+          {
+            rule: "%i == 0",
+            visible: false
+          }
+        ]
+      },
+      guide: {
+        visible: true,
+        lineColor : '#D7D8D9',
+        lineStyle: 'dotted',
+        lineGapSize: '4px',
+        rules: [
+          {
+            rule: "%i == 0",
+            visible: false
+          }
+        ]
+      }
+    },
+    scaleY: {
+      zooming:true,
+      lineWidth: 0,
+      label:{
+        text:"Scale-Y"
+      },
+      item: {
+        padding: "0 10 0 0",
+        fontColor : "#1E5D9E",
+        fontFamily: 'Montserrat',
+        rules: [
+          {
+            rule: "%i == 0",
+            offsetX:10,
+            offsetY:10,
+            backgroundColor:'none'
+          }
+        ]
+      },
+      tick: {
+        lineColor: '#D1D3D4',
+        rules: [
+          {
+            rule: "%i == 0",
+            visible:false
+          }
+        ]
+      },
+      guide: {
+        visible: true,
+        lineColor : '#D7D8D9',
+        lineStyle: 'dotted',
+        lineGapSize: '4px',
+        rules: [
+          {
+            rule: "%i == 0",
+            visible: false
+          }
+        ]
+      }
+    },
+    scrollX:{
+      bar:{
+        backgroundColor:'none',
+        borderLeft:'none',
+        borderTop:'none',
+        borderBottom:'none'
+      },
+      handle:{
+        backgroundColor:'#1E5D9E',
+        height:5
+      }
+    },
+    scrollY:{
+      borderWidth:0,
+      bar:{
+        backgroundColor:'none',
+        width:14,
+        borderLeft:'none',
+        borderTop:'none',
+        borderBottom:'none'
+      },
+      handle:{
+        borderWidth:0,
+        backgroundColor:'#1E5D9E',
+        width:5
+      }
+    },
+    zoom:{
+      backgroundColor:'#1E5D9E',
+      alpha:.33,
+      borderColor:'#000',
+      borderWidth:1
+    },
+    preview:{
+      borderColor:'#1E5D9E',
+      borderWidth:1,
+      adjustLayout:true,
+      handle:{
+        backgroundColor:'#1E5D9E',
+        borderColor:'#1E5D9E'
+      },
+      mask:{
+        backgroundColor:'#FFF',
+        alpha:.95,
+      }
     }
-  },
-  legend: {
-    x: 0,
-    y: 1.0,
-    bgcolor: 'rgba(255, 255, 255, 0)',
-    bordercolor: 'rgba(255, 255, 255, 0)'
-  },
-  barmode: 'group',
-  bargap: 0.15,
-  bargroupgap: 0.1
-};
+  }
+  };
 
-Plotly.newPlot('myDiv', data, layout);
+  var myConfig = {
+  type: "line",
+  title: {
+    text: "Predicted Outputs",
+  },
+  subtitle: {
+    text: "(in million tonnes)",
+  },
+  plotarea: {
+    marginTop:10,
+    marginBottom:80
+  },
+  scaleX: {
+    zooming:true,
+    labels:['6/10','6/11','6/12','6/13','6/14','6/15','6/16','6/17','6/18','6/19','6/20']
+  },
+  scaleY: {
+    zooming:true,
+  },
+  preview:{},
+  scrollX:{},
+  scrollY:{},
+  series : [
+    {
+      text: "Internal",
+      values : [6,6,3.5, 4.5, 4.5, 4.5, 2.5, 4.5, 4.5, 5, 5, 4, 5, 5],
+    },
+  ]
+  };
+
+  zingchart.render({
+  id : 'myChart',
+  data: {
+    gui:{
+      contextMenu:{
+        button:{
+          visible: true,
+          lineColor: "#2D66A4",
+          backgroundColor: "#2D66A4"
+        },
+        gear: {
+          alpha: 1,
+          backgroundColor: "#2D66A4"
+        },
+        position: "right",
+        backgroundColor:"#306EAA", /*sets background for entire contextMenu*/
+        docked: true,
+        item:{
+          backgroundColor:"#306EAA",
+          borderColor:"#306EAA",
+          borderWidth: 0,
+          fontFamily: "Lato",
+          color:"#fff"
+        }
+      }
+    },
+    graphset:[myConfig]
+  },
+  height: 390,
+  width: '100%',
+  defaults: myTheme
+  });
